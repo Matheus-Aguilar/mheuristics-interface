44f79c3683ff935ff8d78c460ec0dde3327bb0ad
Marcos Loures <marcosvcloures@gmail.com>
2017-09-21 13:34:30 -0300

    Deixando mais bonito

--- Interface/View/GeneticAlgorithm.xaml.cs
+++ Interface/View/GeneticAlgorithm.xaml.cs
@@ -19,45 +19,49 @@ namespace Interface.View
     /// <summary>
     /// Interaction logic for GeneticAlgorithm.xaml
     /// </summary>
-    public partial class GeneticAlgorithm : UserControl
+    public partial class GeneticAlgorithm : UserControl, INotifyPropertyChanged
     {
         public Heuristics.HeuristicsBase Heuristics;
         public MainWindow mainWindow;
+        
+        public int populacaoInicial { get; set; }
+        public double taxaCruzamento { get; set; }
+        public double taxaMutacao { get; set; }
+        public int numIteracoes { get; set; }
+
+        public event PropertyChangedEventHandler PropertyChanged;
 
         public GeneticAlgorithm()
         {
+            DataContext = this;
+
+            populacaoInicial = 20;
+            taxaCruzamento = 0.5;
+            taxaMutacao = 0.05;
+            numIteracoes = 400;
+
             InitializeComponent();
         }
 
-        private void StartHeuristics(int populacaoInicial, double taxaCruzamento, double taxaMutacao, int numIteracoes)
+        private void Button_Click(object sender, RoutedEventArgs e)
         {
             BackgroundWorker bkw = new BackgroundWorker();
 
             mainWindow.StartHeuristic();
 
-            mainWindow.Heuristic = new Heuristics.GeneticAlgorithm(populacaoInicial, taxaCruzamento, taxaMutacao, numIteracoes);
+            mainWindow.Heuristic = new Heuristics.GeneticAlgorithm(this.populacaoInicial, taxaCruzamento, taxaMutacao, numIteracoes);
 
-            bkw.DoWork += (sender, e) =>
+            bkw.DoWork += (_, __) =>
             {
                 mainWindow.Heuristic.Run();
             };
 
-            //plotFlorestaRegulada.Series.Add(serieFlorestaRegula);
-
-            bkw.RunWorkerCompleted += (sender, e) =>
+            bkw.RunWorkerCompleted += (_, __) =>
             {
                 mainWindow.EndHeuristic();
             };
 
             bkw.RunWorkerAsync();
-        }
-
-        private void Button_Click(object sender, RoutedEventArgs e)
-        {
-            MainWindow mainWindow = (MainWindow) Window.GetWindow(this);
-
-            StartHeuristics(Convert.ToInt32(populacaoInicial.Text), Convert.ToDouble(taxaCruzamento.Text),
-                Convert.ToDouble(taxaMutacao.Text), Convert.ToInt32(numIteracoes.Text));
         }
     }
 }
